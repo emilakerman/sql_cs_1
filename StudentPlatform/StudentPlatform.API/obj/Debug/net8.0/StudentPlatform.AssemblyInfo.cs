6dfae8784c6911c1212c1704ddb12d179a089a0d
emilakerman <emil.akerman@iths.se>
2023-12-15 14:24:29 +0100

    made the post and get work properly

--- StudentPlatform/StudentPlatform.API/obj/Debug/net8.0/StudentPlatform.AssemblyInfo.cs
+++ StudentPlatform/StudentPlatform.API/obj/Debug/net8.0/StudentPlatform.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentPlatform")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6efa10a590683dcbefa79cc3cb21098ed5497d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentPlatform")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentPlatform")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
